2d08abf75900d6f63c93f1a8b806932e250d2a63
tendo <tuyo1009@gmail.com>
2013-09-26 02:59:24 +0900

    second commit

--- ShootingTest/GameClassViewController.m
+++ ShootingTest/GameClassViewController.m
@@ -10,15 +10,17 @@
 #import "GameClassViewController.h"
 
 
-CGRect rect_frame, rect_myMachine, rect_enemyMachine, rect_myBeam, rect_enemyBeam;
+CGRect rect_frame, rect_myMachine, rect_enemyMachine, rect_myBeam, rect_enemyBeam, rect_beam_launch;
 UIImageView *iv_frame, *iv_myMachine, *iv_enemyMachine, *iv_myBeam, *iv_enemyBeam, *iv_beam_launch;
 
 int x_frame, y_frame;
-int x_myMachine, x_enemyMachine;
-int y_myMachine, y_enemyMachine;
+int x_myMachine, x_enemyMachine, x_beam;
+int y_myMachine, y_enemyMachine, y_beam;
 int size_machine;
-int size_beam;//ビームの長さ
+int length_beam, thick_beam;//ビームの長さと太さ
+
 int center_x;
+int beam_time;
 
 NSTimer *tm;
 float count = 0;
@@ -62,16 +64,17 @@ float count = 0;
     [self.view addSubview:iv_frame];
     
     
-    size_beam = 50;
+    length_beam = 50;
+    thick_beam = 5;
     
 
     size_machine = 30;
-    center_x = [[UIScreen mainScreen] bounds].size.width/2 - size_machine/2;//画面サイズに対して中央になるように左位置特定
+    center_x = rect_frame.size.width/2 - size_machine/2;//画面サイズに対して中央になるように左位置特定
     x_myMachine = center_x;//自機横位置は中心
     y_myMachine = 300;//自機縦位置
     x_enemyMachine = center_x;//敵機横位置は中心
     y_enemyMachine = 50;//敵機縦位置
-
+    iv_myBeam = nil;//ビームは初期状態でnil
     
     count = 0;
     tm = [NSTimer scheduledTimerWithTimeInterval:0.1
@@ -132,7 +135,23 @@ float count = 0;
     [iv_enemyMachine startAnimating];
     
     
-    
+    //ビーム進行
+    [iv_myBeam removeFromSuperview];
+    if(iv_myBeam != nil){//ビームが枠外に出るか敵に命中したらiv_myBeamをnilにする
+        [iv_myBeam removeFromSuperview];
+        rect_myBeam = CGRectMake(x_beam, y_beam, thick_beam, length_beam);
+        iv_myBeam = [[UIImageView alloc]initWithFrame:rect_myBeam];
+        iv_myBeam.image = [UIImage imageNamed:@"beam.png"];
+        [self.view addSubview:iv_myBeam];
+        x_beam = x_myMachine + size_machine / 2;
+        y_beam -= length_beam;
+        
+        if(y_beam < - length_beam){
+            
+            [iv_myBeam removeFromSuperview];
+            iv_myBeam = nil;
+        }
+    }
     
     
 
@@ -161,9 +180,11 @@ float count = 0;
 - (void)time:(NSTimer*)timer{
     count += 0.1;
     
-    //前時刻の敵機を消去
+    //前時刻の描画を消去
     [iv_enemyMachine removeFromSuperview];
     [iv_myMachine removeFromSuperview];
+//    [iv_beam_launch removeFromSuperview];
+    
     
 //    NSLog(@"time:%f", count);
     //タイマーが有効かどうか
@@ -171,19 +192,22 @@ float count = 0;
     //    NSLog(@"isValid:%@", str);
     srand(time(nil));
 //    int x_move = rand() % 50 - 25;
-    int x_move = arc4random() % size_machine - size_machine / 2;
+//    int x_move = arc4random() % size_machine - size_machine / 2;
+    int x_move = center_x * (sin(2 * M_PI * count * 50/ 360.0f) + 1.0f);
+//    NSLog(@"%f", sin(2 * M_PI * count * 50/ 360.0f)/2.0f);
 //    NSLog(@"x_move = %d", x_move);
-    x_enemyMachine = x_enemyMachine + x_move;
+//    x_enemyMachine = x_enemyMachine + x_move;
+    x_enemyMachine = x_move;//正弦波の場合
     if (x_enemyMachine < 0){
         x_enemyMachine = size_machine * 2;
-    }else if(x_enemyMachine > [[UIScreen mainScreen] bounds].size.width - size_machine - 25){
-        x_enemyMachine = [[UIScreen mainScreen] bounds].size.width - size_machine * 2;
+    }else if(x_enemyMachine > x_frame - size_machine){
+        x_enemyMachine = x_frame - size_machine * 2;
     }
 //    NSLog(@"x_enemy = %d", x_enemyMachine);
     [self ordinaryAnimationStart];
     
     //一定時間経過するとゲームオーバー
-    if(count >=15){
+    if(count >=150){
         NSLog(@"gameover");
         //３秒経過したらタイマー終了
         [tm invalidate];
@@ -218,13 +242,46 @@ float count = 0;
 //        }];
         
         if (point.y < 0) {//上向き
-            NSLog(@"beam!!");
             
-            CGRect rect_beam_launch = CGRectMake(x_myMachine + size_machine / 2, y_myMachine - size_beam, 5, size_beam);
-            iv_beam_launch = [[UIImageView alloc]initWithFrame:rect_beam_launch];
-            iv_beam_launch.image = [UIImage imageNamed:@"beam.png"];
-            [self.view addSubview:iv_beam_launch];
-            
+            if(iv_myBeam == nil){
+                NSLog(@"beam!!");
+                
+                //ビーム発射=>最終的には煙幕のようなものを出す
+                rect_beam_launch = CGRectMake(x_myMachine + size_machine / 2,
+                                              y_myMachine - length_beam,
+                                              thick_beam,
+                                              length_beam);
+                iv_beam_launch = [[UIImageView alloc]initWithFrame:rect_beam_launch];
+                NSMutableArray *_beamImageList = [[NSMutableArray alloc] init];
+                [_beamImageList addObject:[UIImage imageNamed:[NSString stringWithFormat:@"beam.png"]]];
+                [_beamImageList addObject:[UIImage imageNamed:[NSString stringWithFormat:@"nothing32.png"]]];
+                //            iv_beam_launch = [[UIImageView alloc]initWithFrame:rect_beam_launch];
+                iv_beam_launch.animationImages = _beamImageList;
+                iv_beam_launch.animationDuration = 0.5;//0.5秒間隔でアニメーション
+                iv_beam_launch.animationRepeatCount = 3;//切り替え回数
+                [self.view addSubview:iv_beam_launch];
+                NSLog(@"%@", self);
+                [iv_beam_launch startAnimating];
+                
+                
+                
+                //ビーム進行
+                x_beam = x_myMachine + size_machine / 2;
+                y_beam = y_myMachine - length_beam,
+                rect_myBeam = CGRectMake(x_beam,
+                                         y_beam,
+                                         thick_beam,
+                                         length_beam);
+                iv_myBeam = [[UIImageView alloc]initWithFrame:rect_myBeam];
+                iv_myBeam.image = [UIImage imageNamed:@"beam.png"];
+                [self.view addSubview:iv_myBeam];
+                
+                
+                beam_time = 0;
+
+            }else{
+                NSLog(@"%@", iv_myBeam);
+            }
         }
         else {//下向き
             NSLog(@"back");
@@ -244,5 +301,13 @@ float count = 0;
     
 }
 
-
+-(void) viewWillDisappear:(BOOL)animated {
+    if ([self.navigationController.viewControllers indexOfObject:self]==NSNotFound) {
+        // back button was pressed.  We know this is true because self is no longer
+        // in the navigation stack.
+        NSLog(@"pressed back button");
+        [tm invalidate];
+    }
+    [super viewWillDisappear:animated];
+}
 @end
